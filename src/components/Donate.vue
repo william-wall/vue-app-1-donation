7dfbf6dd7f303fd167c3c7699bd72d5af99c26d5
william-wall <william@williamwall.ie>
2018-07-24 13:19:02 +0100

    initial commit

---
+++ src/components/Donate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hero">
+    <h3 class="vue-title"><i class="fa fa-money" style="padding: 3px"></i>{{messagetitle}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Donate',
+  data () {
+    return {
+      messagetitle: ' Donate '
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .vue-title {
+    margin-top: 30px;
+    text-align: center;
+    font-size: 45pt;
+    margin-bottom: 10px;
+  }
+</style>
